dcdbba341b9e1c08e04f6737c742bbf6647475d0
yirubal <yiruubal@gmail.com>
2025-08-19 12:33:50 +0300

    feat:add project setup with neccesary app registration

---
+++ tera/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for tera project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path("api/protectors/", include("protector.urls")),
+    # path("api/drivers/", include("driver.urls")),
+    # path("api/terminals/", include("terminal.urls")),
+    # path("api/turns/", include("queue.urls")),
+    # path("api/departures/", include("departure.urls")),
+    # path("api/shift/", include("shift.urls")),
+    # path("api/routes/", include("route.urls")),
+    # path("api/earnings/", include("earnings.urls")),
+    # path("api/analytics/", include("analytics.urls")),
+
+]
