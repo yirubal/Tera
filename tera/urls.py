729e01fe675c74d40e862ddaf16de5f00354368f
yirubal <yiruubal@gmail.com>
2025-08-28 16:30:17 +0300

    refactor(urls): clean up commented-out API routes

--- tera/urls.py
+++ tera/urls.py
@@ -21,15 +21,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path("api/protectors/", include("protector.urls")),
-    # path("api/drivers/", include("driver.urls")),
-    # path("api/terminals/", include("terminal.urls")),
-    # # path("api/turns/", include("queue.urls")),
-    # path("api/departures/", include("departure.urls")),
-    # path("api/shift/", include("shift.urls")),
-    # path("api/routes/", include("route.urls")),
-    # path("api/earnings/", include("earnings.urls")),
-    # path("api/analytics/", include("analytics.urls")),
+
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/protector/", include("protector.urls")),
